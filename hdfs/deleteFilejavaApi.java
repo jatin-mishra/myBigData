c6233b6e4e195caf09a6c022ff2439ed7afd77ca
Jatin Mishra <44506861+jatin-mishra@users.noreply.github.com>
2020-06-18 23:53:03 +0530

    handsOn done

--- hdfs/deleteFilejavaApi.java
+++ hdfs/deleteFilejavaApi.java
@@ -1,18 +1,20 @@
-package packageName;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
-import requiredModules;
+import java.io.*;
 
-public class DeleteHdfsFile{
-
+public class deleteDir{
 	public static void main(String[] args) throws Exception{
-		
-		String uri = "hdfs://localhost:9000/dirname/filename";		
+
+		if(args.length != 1){
+			System.err.println("Usage: deleteDir <path>");
+			System.exit(0);
+		}
+
 		Configuration conf = new Configuration();
-		FileSystem fs = FileSystem.get(URI.create(uri),conf);
-		fs.delete(new Path(uri),true);
+		FileSystem fs = FileSystem.get(conf);
+
+		fs.delete(new Path(args[0]),true);
 	}
-
 }
-
-
-
